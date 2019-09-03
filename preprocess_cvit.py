b2485132ecd84e4431c3f3d8f10e98a0241b7d75
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-09-04 02:23:50 +0530

    Cleaning up

--- preprocess_cvit.py
+++ preprocess_cvit.py
@@ -4,11 +4,8 @@ from fairseq.data.cvit.dataset import _CVITIndexedRawTextDataset
 from fairseq.data.cvit.lmdb import LMDBCorpusWriter, LMDBCorpus
 import yaml
 from multiprocessing import Pool
+from functools import partial
 import os
-
-def add_args(parser):
-	parser.add_argument('data', help='colon separated path to data directories list, \
-						will be iterated upon during epochs in round-robin manner')
 
 def read_config(path):
     with open(path) as config:
@@ -16,46 +13,46 @@ def read_config(path):
         data = yaml.load(contents)
         return data
 
-#data = read_config(args.data)
-def build_corpus(corpus):
-	from ilmulti.sentencepiece import SentencePieceTokenizer
-	tokenizer = SentencePieceTokenizer()
-	if not LMDBCorpus.exists(corpus):
-		print("LMDB({}) does not exist. Building".format(corpus.path))
-		raw_dataset = _CVITIndexedRawTextDataset(corpus, tokenizer)
-		# writer = BufferedLMDBCorpusWriter(corpus, tokenizer, num_workers=30, max_size=1024*1024)
-		writer = LMDBCorpusWriter(raw_dataset)
-		writer.close()
-		print("Built LMDB({})".format(corpus.path))
+def build_corpus(corpus, rebuild=False):
+    from ilmulti.sentencepiece import SentencePieceTokenizer
+    tokenizer = SentencePieceTokenizer()
+    if not LMDBCorpus.exists(corpus):
+        print("LMDB({}) does not exist. Building".format(corpus.path))
+        raw_dataset = _CVITIndexedRawTextDataset(corpus, tokenizer)
+        writer = LMDBCorpusWriter(raw_dataset)
+        writer.close()
+        print("Built LMDB({})".format(corpus.path))
 
 
 def get_pairs(data):
-	corpora = []
-	for split in ['train','dev','test']:
-		pairs = pairs_select(data['corpora'], split) 
-		#print(pairs)
-		srcs,tgts = list(zip(*pairs))
-		corpora.extend(srcs)
-		corpora.extend(tgts)
-	
-	return list(set(corpora))
+    corpora = []
+    for split in ['train','dev','test']:
+        pairs = pairs_select(data['corpora'], split) 
+        #print(pairs)
+        srcs,tgts = list(zip(*pairs))
+        corpora.extend(srcs)
+        corpora.extend(tgts)
+    
+    return list(set(corpora))
+
+def main(config_file, rebuild):
+    data = read_config(config_file)
+    corpora = get_pairs(data)
+    build_corpus_ = partial(build_corpus, rebuild=rebuild)
+
+    # Create pool of processes eqvt to cores
+    # Parallel call build_corpus on corpora
+    cores = os.cpu_count()
+    pool = Pool(processes=cores)
+    pool.map_async(build_corpus_, corpora)
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
-	parser=ArgumentParser()
-	parser.add_argument('data')
-	args = parser.parse_args()
-	data = read_config(args.data)
-	#print(data) 
-	corpora=get_pairs(data)
-
-	# Create pool of processes eqvt to cores
-	# Parallel call build_corpus on corpora
-	pool = Pool(processes=os.cpu_count())
-	pool.map_async(build_corpus , corpora)
-	pool.close()
-	pool.join()
-
-
-
-      
+    parser = ArgumentParser()
+    parser.add_argument('config_file', help='config file')
+    parser.add_argument('--rebuild', action='store_true')
+    
+    args = parser.parse_args()
+    main(args.config_file, args.rebuild)
